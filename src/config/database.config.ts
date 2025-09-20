36f9b1eb31d6853448eef4c6de18580880ca43ba
Shady Rajab <shadyrajaab@gmail.com>
2025-09-20 01:05:57 -0300

    feat: add envs

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,13 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../modules/users/entities/user.entity';
+import { DB_NAME, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './constants';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'admin',
-  password: 'admin',
-  database: 'ordersdb',
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
   entities: [User],
   synchronize: true,
 };
